56fb7a958dbabce5d964a2caf7a0033c0420f814
Kyle Scudder <kyle.scudder@crimsontide.co.uk>
2025-09-16 20:21:30 +0100

    chore: unversioned file

---
+++ lib/constants.ts
@@ -0,0 +1,9 @@
+// Centralized app-wide constants
+
+export const STORAGE_KEYS = {
+  SELECTED_PROJECT: 'jira-clone-selected-project',
+  SELECTED_SPRINTS: 'jira-clone-selected-sprints',
+  FILTERS: 'jira-clone-filters',
+  FILTER_SIDEBAR_OPEN: 'jira-clone-filter-sidebar-open',
+  FILTER_SECTIONS: 'jira-clone-filter-sections'
+} as const
